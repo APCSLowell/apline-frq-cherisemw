2a19eb6bc6c42056bfd8cdbd9b2328d5813f52e5
cherisemw <86027010+cherisemw@users.noreply.github.com>
2024-04-24 13:33:18 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -6,7 +6,7 @@ public class APLine
   private int myB;
   private int myC;
 
-  public void APLine(int a, int b, int c){
+  public APLine(int a, int b, int c){
     myA = a; 
     myB = b;
     myC = c;
